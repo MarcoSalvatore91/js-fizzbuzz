5103db325212f016c40bd2a9e1ffd482ba38d8d4
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2021-12-22 15:58:57 +0100

    written a program with multiples

--- js/script.js
+++ js/script.js
@@ -4,9 +4,28 @@ console.log('JS OK!');
 ma per i multipli di 3 stampi Fizz al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
-// 1- 
-//
-//
-//
-//
-//
+// 1- Scrivere un programma che stampi i numeri da 1 a 100
+// 2- Per i multipli di 3 stampare la parola Fizz 
+// 3- Per i multipli di 5 stampare la parola Buzz 
+// 4- Per i multipli di 3 e 5 stampare la parola FizzBuzz 
+
+const numberList = document.getElementById('number');
+
+let elementNumber = ' ';
+
+// 1- Scrivere un programma che stampi i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    // 2- Per i multipli di 3 stampare la parola Fizz 
+    if (i % 3 === 0) {
+        console.log('Fizz')
+    // 3- Per i multipli di 5 stampare la parola Buzz 
+    } if (i % 5 === 0) {
+        console.log('Buzz')
+    // 4- Per i multipli di 3 e 5 stampare la parola FizzBuzz
+    } if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz')
+    } else {
+        elementNumber += ` ${i}`;
+        console.log(elementNumber);
+    }
+}
