730f5a95606a7dbcbeb08ff37642f97d3bfbf179
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2021-12-23 18:00:16 +0100

    end project

--- js/script.js
+++ js/script.js
@@ -5,9 +5,10 @@ ma per i multipli di 3 stampi Fizz al posto del numero e per i multipli di 5 sta
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
 // 1- Scrivere un programma che stampi i numeri da 1 a 100
-// 2- Per i multipli di 3 stampare la parola Fizz 
-// 3- Per i multipli di 5 stampare la parola Buzz 
-// 4- Per i multipli di 3 e 5 stampare la parola FizzBuzz 
+// 2- Per i multipli di 3 e 5 stampare la parola FizzBuzz 
+// 3- Per i multipli di 3 stampare la parola Fizz 
+// 4- Per i multipli di 5 stampare la parola Buzz 
+// 5- Stampare il codice
 
 const numberList = document.getElementById('number');
 
@@ -15,19 +16,22 @@ let elementNumber = ' ';
 
 // 1- Scrivere un programma che stampi i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    // 2- Per i multipli di 3 stampare la parola Fizz 
+    // 2- Per i multipli di 3 e 5 stampare la parola FizzBuzz 
     if (i % 3 === 0 && i % 5 === 0) {
+        elementNumber += `<li id"number"> FizzBuzz</li>`;
         console.log('FizzBuzz')
-    // 3- Per i multipli di 5 stampare la parola Buzz 
-    } if (i % 3 === 0 && !(i % 5 === 0)) {
+    // 3- Per i multipli di 3 stampare la parola Fizz
+    } else if (i % 3 === 0 && !(i % 5 === 0)) {
+        elementNumber += `<li id"number"> Fizz</li>`;
         console.log('Fizz')
-    // 4- Per i multipli di 3 e 5 stampare la parola FizzBuzz
-    } if (i % 5 === 0 && !(i % 3 === 0)) {
+    // 4- Per i multipli di 5 stampare la parola Buzz
+    } else if (i % 5 === 0 && !(i % 3 === 0)) {
+        elementNumber += `<li id"number"> Buzz</li>`;
         console.log('Buzz')
     } else {
-        elementNumber += ` ${i}`;
-        console.log(elementNumber);
+        elementNumber += `<li id"number"> ${i}</li>`;
+        console.log(i);
     }
 }
-
-numberList.innerText = elementNumber;
+// Stampare il codice
+numberList.innerHTML = elementNumber;
